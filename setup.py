bd4ba706bf7479c278e04cf49be981e28baca31a
HarveyPrew <harvey.prewer@gmail.com>
2022-12-22 20:04:07 +0000

    renaming in setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="alan_pytridge",
+    name="DJSetFiller",
     packages=find_packages(where='src'),
     include_package_data=True,
     package_dir={'': 'src'},
