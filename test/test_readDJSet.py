8c94125c0e74a0f718d27362e2a8e27cc5bc006e
HarveyPrew <harvey.prewer@gmail.com>
2022-12-25 18:30:26 +0000

    another commit reccomendations

--- test/test_readDJSet.py
+++ test/test_readDJSet.py
@@ -7,3 +7,8 @@ def test_read_DJ_set_id():
         djset.id
         == "2015-01-02 - Floating Points & Four Tet @ Plastic People Closing Party, London"
     )
+
+
+def test_DJ_set_reccomendation_contains_songs():
+    djset = DJSet("DJSetInput.txt")
+    assert len(djset.recomendations) > 0
