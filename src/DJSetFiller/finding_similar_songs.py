37c48d762a2567a28677d9cab1ac13416fbe15e8
HarveyPrew <harvey.prewer@gmail.com>
2023-01-29 10:39:58 +0000

    song finder worked

--- src/DJSetFiller/finding_similar_songs.py
+++ src/DJSetFiller/finding_similar_songs.py
@@ -1,6 +1,22 @@
 import pandas as pd
+import os
 
 
 def read_data_set():
     collab_df = pd.read_csv("data/mixesdb_df_for_recs.csv")
+    # needed to make the training not take ages
+    os.environ["MKL_NUM_THREADS"] = "1"
     return collab_df
+
+
+def collab_filter(song_id, user_song_df, num_songs=5):
+    """
+    song_id = spotify id for individual song
+    user_song_df= dataframe with users, songs, playcounts etc
+    for the time being i am not going to enable filtering by key/tempo as not enough songs
+    but in future will do
+    """
+
+    song_num = user_song_df[user_song_df.spotify_id == song_id].song_nums.values[0]
+
+    return song_num
--- test/test_finding_similar_songs.py
+++ test/test_finding_similar_songs.py
@@ -1,6 +1,11 @@
-from DJSetFiller.finding_similar_songs import read_data_set
+from DJSetFiller.finding_similar_songs import read_data_set, collab_filter
 
 
 def test_find_db_exists():
     collab_df = read_data_set()
     assert collab_df is not None
+
+
+def test_find_song_num():
+    song_num = collab_filter("6vXcHqE3bYPirOgKlDbN5s", read_data_set())
+    assert song_num == 43414
