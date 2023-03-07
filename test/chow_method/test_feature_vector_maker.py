f95152f3d4154a546c31d9bd64b37f68f98593be
HarveyPrew <harvey.prewer@gmail.com>
2023-03-07 14:28:13 +0000

    separate out vector maker tests

---
+++ test/chow_method/test_feature_vector_maker.py
@@ -0,0 +1,88 @@
+from DJSetFiller.feature_vector_maker import (
+    input_feature_vector,
+    feature_vectors_to_dict,
+    scaler
+)
+
+from DJSetFiller.inital_suggestions import (
+    multiple_song_input_reccomender
+)
+
+
+def test_input_vector():
+    uri_list = ["5vXlU52ohBRZb1uUw4GPqA", "5Zdmkal4CNnC5EY9qCSrMi"]
+    initial_suggestions = multiple_song_input_reccomender(uri_list)
+    output = input_feature_vector(initial_suggestions)
+
+    assert output == [
+        0.6145,
+        0.705,
+        2.5,
+        -9.8355,
+        0.0,
+        0.057800000000000004,
+        0.015175000000000001,
+        0.5955,
+        0.3855,
+        0.16165,
+        128.481,
+        4.0,
+    ]
+
+
+def test_output_vector():
+    uri_list = ["5vXlU52ohBRZb1uUw4GPqA", "5Zdmkal4CNnC5EY9qCSrMi"]
+    initial_suggestions = multiple_song_input_reccomender(uri_list)
+    output = feature_vectors_to_dict(initial_suggestions)
+
+    assert output[4.0][00] == 0.817
+
+
+def test_scaler():
+    vectors = {
+        0: [
+            0.817,
+            0.975,
+            4.0,
+            -6.946,
+            0.0,
+            0.0681,
+            0.000677,
+            0.806,
+            0.0904,
+            0.125,
+            120.013,
+            4.0,
+        ],
+        1: [
+            0.729,
+            0.481,
+            10.0,
+            -15.041,
+            0.0,
+            0.0699,
+            0.000392,
+            0.827,
+            0.13,
+            0.649,
+            130.188,
+            4.0,
+        ],
+    }
+
+    output = scaler(vectors)
+
+    assert output[0] == [
+        1.0000000000000013,
+        1.0,
+        -1.0,
+        1.0000000000000002,
+        0.0,
+        -1.0000000000000078,
+        0.9999999999999997,
+        -1.0,
+        -0.9999999999999997,
+        -1.0,
+        -1.0,
+        0.0,
+    ]
--- test/chow_method/test_finding_similar_songs.py
+++ test/chow_method/test_finding_similar_songs.py
@@ -1,15 +1,9 @@
-import beavis
-from io import StringIO
-import pandas as pd
 from DJSetFiller.finding_similar_songs import (
     read_data_set,
     multiple_song_input_reccomender,
     matrix_size,
-    input_feature_vector,
-    feature_vectors_to_dict,
     euclidean_distance,
     reduced_similar_songs,
-    scaler,
     calculated_eds,
     input_test_songs,
     organise_missing_songs
@@ -66,85 +60,6 @@ def test_smallest_ed():
     best_songs = reduced_similar_songs(initial_suggestions)
     song_names = best_songs["song"].values.tolist()
     assert any("State Of Rave (Viers Remix)" in name for name in song_names)
-
-
-def test_scaler():
-    vectors = {
-        0: [
-            0.817,
-            0.975,
-            4.0,
-            -6.946,
-            0.0,
-            0.0681,
-            0.000677,
-            0.806,
-            0.0904,
-            0.125,
-            120.013,
-            4.0,
-        ],
-        1: [
-            0.729,
-            0.481,
-            10.0,
-            -15.041,
-            0.0,
-            0.0699,
-            0.000392,
-            0.827,
-            0.13,
-            0.649,
-            130.188,
-            4.0,
-        ],
-    }
-
-    output = scaler(vectors)
-
-    assert output[0] == [
-        1.0000000000000013,
-        1.0,
-        -1.0,
-        1.0000000000000002,
-        0.0,
-        -1.0000000000000078,
-        0.9999999999999997,
-        -1.0,
-        -0.9999999999999997,
-        -1.0,
-        -1.0,
-        0.0,
-    ]
-
-
-def test_input_vector():
-    uri_list = ["5vXlU52ohBRZb1uUw4GPqA", "5Zdmkal4CNnC5EY9qCSrMi"]
-    initial_suggestions = multiple_song_input_reccomender(uri_list, read_data_set())
-    output = input_feature_vector(initial_suggestions)
-
-    assert output == [
-        0.6145,
-        0.705,
-        2.5,
-        -9.8355,
-        0.0,
-        0.057800000000000004,
-        0.015175000000000001,
-        0.5955,
-        0.3855,
-        0.16165,
-        128.481,
-        4.0,
-    ]
-
-
-def test_output_vector():
-    uri_list = ["5vXlU52ohBRZb1uUw4GPqA", "5Zdmkal4CNnC5EY9qCSrMi"]
-    initial_suggestions = multiple_song_input_reccomender(uri_list, read_data_set())
-    output = feature_vectors_to_dict(initial_suggestions)
-
-    assert output[4.0][00] == 0.817
 
 
 def test_calulate_ed():
