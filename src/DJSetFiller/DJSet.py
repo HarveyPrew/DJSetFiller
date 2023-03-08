fee6a2568c756b5d9b130f3eb3aa17b493697edd
HarveyPrew <harvey.prewer@gmail.com>
2023-03-08 14:01:47 +0000

    reading withheld songs successfully

--- src/DJSetFiller/DJSet.py
+++ src/DJSetFiller/DJSet.py
@@ -1,6 +1,36 @@
-class DJSet:
-    def __init__(self):
-        return
+import pandas as pd
 
+
+class DJSet:
+    def __init__(self, dj_set_id):
+        self.dj_set_id = dj_set_id
+        self.missing_songs = set()
+
+    def add_missing_song(self, song_id):
+        self.missing_songs.add(song_id)
+
+    # R-precision is the number of retrieved relevant tracks divided by
+    # the number of known relevant tracks (i.e., the number of withheld tracks)
     def calculate_r_precision(self):
         return 0.5
+
+    # Number of songs we withheld for this DJ set.
+    def number_of_known_relevant_tracks(self):
+        return len(self.missing_songs)
+
+    def read_withheld_songs(path):
+        dj_sets = {}
+
+        raw_data = pd.read_csv(path)
+
+        for _, row in raw_data.iterrows():
+
+            dj_set_id = row["title + dj"]
+            song_id = row["spotify_id"]
+
+            if dj_set_id not in dj_sets:
+                dj_sets[dj_set_id] = DJSet(dj_set_id)
+
+            dj_sets[dj_set_id].add_missing_song(song_id)
+
+        return dj_sets
--- test/chow_method/test_r_precision.py
+++ test/chow_method/test_r_precision.py
@@ -3,7 +3,36 @@ from DJSetFiller.DJSet import DJSet
 
 def test_r_precision():
     expected_r_precision = 0.5
-    djset = DJSet()
+    djset = DJSet("a")
     actual_r_precision = djset.calculate_r_precision()
 
     assert actual_r_precision == expected_r_precision
+
+
+def test_number_of_known_relevant_tracks():
+    expected_number_of_known_relevant_tracks = 1
+    djset = DJSet("a")
+
+    djset.add_missing_song(1)
+    actual_number_of_known_relevant_tracks = djset.number_of_known_relevant_tracks()
+
+    assert (
+        expected_number_of_known_relevant_tracks
+        == actual_number_of_known_relevant_tracks
+    )
+
+
+def test_read_withheld_songs():
+    expected_number_of_known_relevant_tracks = 3
+    dj_set_id = ' - ≈Åukasz Tomaszewski, ¬°MASH-UP!, Siriusmo, Lionza, Michael Mayer - COSMO Selektor 1692'
+    djsets = DJSet.read_withheld_songs("data/rprecision_data/reduced/missing_songs_reduced.csv")
+    test_dj_set = djsets[dj_set_id]
+
+    actual_number_of_known_relevant_tracks = test_dj_set.number_of_known_relevant_tracks()
+
+    assert (
+        expected_number_of_known_relevant_tracks
+        == actual_number_of_known_relevant_tracks
+    )
+
+ 
