9003b9c24a8c569cdeab901a1ef3c6bf4431c12f
HarveyPrew <harvey.prewer@gmail.com>
2023-03-08 11:51:21 +0000

    deleting unused files

--- test/chow_method/test_class_test.py
+++
@@ -1,6 +0,0 @@
-from DJSetFiller.class_test import dog_speakers
-
-
-def test_dog_speakers():
-    dogs = dog_speakers()
-    assert dogs is not None
--- test/spotipy_tests/test_quickstart.py
+++
@@ -1,7 +0,0 @@
-from DJSetFiller.QuickStart import quick_start
-
-
-def test_quickstart_occurs():
-    id = "5vXlU52ohBRZb1uUw4GPqA"
-    quickstart = quick_start(id)
-    assert quickstart is not None
