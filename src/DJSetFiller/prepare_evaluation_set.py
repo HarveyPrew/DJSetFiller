6316ad0de00d5d97cff48bee83e8449d6150a0a2
HarveyPrew <harvey.prewer@gmail.com>
2023-03-11 14:19:26 +0000

    eadded makes copy to 10%

--- src/DJSetFiller/prepare_evaluation_set.py
+++ src/DJSetFiller/prepare_evaluation_set.py
@@ -1,14 +1,18 @@
 import pandas as pd
 
 
+def create_missing_songs(chosen_sets):
+    return
+
+
 def select_ten_percent_of_sets(path):
     sets_with_multiple_plays = find_sets_with_multiple_plays(path)
     unique_sets = sets_with_multiple_plays.drop_duplicates(subset='set_name_plus_dj_id').copy()
     percentage_of_sets = unique_sets.sample(frac=0.333, replace=True, random_state=1)
     unique_sets_list = percentage_of_sets['set_name_plus_dj_id'].tolist()
 
-    ten_percent = sets_with_multiple_plays[sets_with_multiple_plays['set_name_plus_dj_id'].isin(unique_sets_list)]
-    return ten_percent
+    chosen_sets = sets_with_multiple_plays[sets_with_multiple_plays['set_name_plus_dj_id'].isin(unique_sets_list)].copy()
+    return chosen_sets
 
 
 def find_sets_with_multiple_plays(path):
--- test/chow_method/test_prepare_evaluation_set.py
+++ test/chow_method/test_prepare_evaluation_set.py
@@ -1,4 +1,8 @@
-from DJSetFiller.prepare_evaluation_set import import_csv, find_sets_with_multiple_plays, select_ten_percent_of_sets
+from DJSetFiller.prepare_evaluation_set import (
+    import_csv, 
+    find_sets_with_multiple_plays, 
+    select_ten_percent_of_sets,
+    create_missing_songs)
 
 
 def test_dataset_is_imported():
@@ -18,4 +22,11 @@ def test_filter_sets_with_multiple_plays():
 def test_select_ten_percent_of_sets():
     expected_row_amount = 8
     actual_row_amount = len(select_ten_percent_of_sets("data/rprecision_data/reduced/data_set_test_reduced.csv"))
+    assert actual_row_amount == expected_row_amount
+
+
+def test_create_missing_songs():
+    expected_row_amount = 4
+    chosen_sets = select_ten_percent_of_sets("data/rprecision_data/reduced/data_set_test_reduced.csv")
+    actual_row_amount = len(create_missing_songs(chosen_sets))
     assert actual_row_amount == expected_row_amount
