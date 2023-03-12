3532ee88c47ad7bfaeba7721109b363ce089d669
HarveyPrew <harvey.prewer@gmail.com>
2023-03-12 22:36:54 +0000

    running way quicker

--- src/DJSetFiller/spotify_analysis.py
+++ src/DJSetFiller/spotify_analysis.py
@@ -7,15 +7,22 @@ class SpotifyAPI(object):
     def __init__(self):
         self.auth_manager = SpotifyClientCredentials()
         self.spotify_client = spotipy.Spotify(auth_manager=self.auth_manager)
+        self.response_cache = {}
 
     def get_spotify_features(self, similar_songs):
         recommendedSongs = similar_songs.reset_index(drop=True)
         analysis = []
 
-        for id in recommendedSongs.index:
-            analysis += self.spotify_client.audio_features(
-                recommendedSongs["spotify_id"][id]
-            )
+        for song_spotify_id in recommendedSongs.index:
+
+            if song_spotify_id not in self.response_cache.keys():
+                self.response_cache[
+                    song_spotify_id
+                ] = self.spotify_client.audio_features(
+                    recommendedSongs["spotify_id"][song_spotify_id]
+                )
+
+            analysis += self.response_cache[song_spotify_id]
 
         song_features = pd.DataFrame.from_dict(analysis).drop(
             ["id", "analysis_url", "track_href", "uri", "type"], axis=1
